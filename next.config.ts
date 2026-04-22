0472ff5b3ecfc5856f2d18504ff591618f5fb14b
Sharafath Ali <sharafathaliofficial@gmail.com>
2026-04-22 17:56:57 +0530

    feat: configure next/image to allow remote patterns for external hostnames

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // allow any external hostname (user-submitted URLs)
+      },
+      // {
+      //   protocol: "http",
+      //   hostname: "**",
+      // },
+    ],
+  },
 };
 
 export default nextConfig;
